e30fab58ba04eb070731f79ca18a7b8de19a6419
Ravi Paribrajak <paribrajakravishankarkumar@gmail.com>
2026-06-23 16:50:18 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.py
@@ -0,0 +1,28 @@
+from math import ceil
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        low = 1
+        high = max(piles)
+        res = high
+
+        while low <= high:
+            mid = (low + high) // 2
+            total_hours = 0
+
+            for pile in piles:
+                total_hours += ceil(pile / mid)
+
+            if(total_hours <= h):
+                res = mid
+                high = mid - 1
+
+            else:
+                low = mid + 1   
+        return res
+
+            
+
+
+
+
---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        target_row = 0
+        start_row = 0
+        end_row = len(matrix) - 1
+        while start_row <= end_row:
+            mid = (start_row + end_row) // 2
+            row_start = matrix[mid][0]
+            row_end = matrix[mid][-1]
+            if row_start <= target <= row_end:
+                target_row = mid
+                break
+            elif row_start > target:
+                end_row = mid - 1
+            else: 
+                start_row = mid + 1
+
+        low = 0
+        high = len(matrix[target_row]) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            item = matrix[target_row][mid]
+            if item == target:
+                return True
+            elif item > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return False
